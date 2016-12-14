ee437d06daac73377ff76830daa2622b9de7c8b4
yourUsername <your_email@example.com>
2016-12-14 14:32:57 -0500

    files created and tables setup

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS pokemons;
+DROP TABLE IF EXISTS trainers;
+
+
+CREATE TABLE pokemons (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  poke_type VARCHAR(255),
+  cp INTEGER,
+  img_url VARCHAR (512)
+);
+
+CREATE TABLE trainers (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  level INTEGER,
+  trainer_id INTEGER REFERENCES trainers(id),
+  img_url VARCHAR(512)
+);
